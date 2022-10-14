01552ccb41129b9ad15bb613f6165c005bc127e8
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-10-14 20:18:21 +0530

    Added solution - LeetHub

---
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find largest rectangular area possible in a given histogram.
+    long long getMaxArea(long long arr[], int n)
+    {
+        // Your code here
+        stack<pair<long ,long>>s,s1;
+        vector<int>right,left;
+        vector<long long>width(n),area(n);
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0){
+                right.push_back(n);
+            }
+            else if(s.size()>0 && s.top().first<arr[i]){
+                right.push_back(s.top().second);
+            }
+            else if(s.size()>0 && s.top().first>=arr[i]){
+                while(s.size()>0 && s.top().first>=arr[i])
+                s.pop();
+                if(s.size()==0)
+                right.push_back(n);
+                else
+                right.push_back(s.top().second);
+            }
+            s.push({arr[i],i});
+        }
+        reverse(right.begin(),right.end());
+        for(int i=0;i<n;i++){
+            if(s1.size()==0){
+                left.push_back(-1);
+            }
+            else if(s1.size()>0 && s1.top().first<arr[i]){
+                left.push_back(s1.top().second);
+            }
+            else if(s1.size()>0 && s1.top().first>=arr[i]){
+                while(s1.size()>0 && s1.top().first>=arr[i])
+                s1.pop();
+                if(s1.size()==0)
+                left.push_back(-1);
+                else
+                left.push_back(s1.top().second);
+            }
+            s1.push({arr[i],i});
+        }
+        for(int i=0;i<n;i++){
+            width[i]=right[i]-left[i]-1;
+        }
+        long long maxproduct=0;
+        for(int i=0;i<n;i++){
+           area[i]=width[i]*arr[i];
+        }
+        for(int i=0;i<n;i++){
+            if(area[i]>maxproduct){
+                maxproduct=area[i];
+            }
+        }
+        return maxproduct;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.getMaxArea(arr, n)<<endl;
+    
+    }
+	return 0;
+}
+
+// } Driver Code Ends

7f348a14737140d647439c75e7db2c79118603ac
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-10-28 11:40:25 +0530

    Time: 81 ms (43.22%), Space: 19.5 MB (84.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
-    string generate(string &s) {
-        int count[26] = {0};
-        
-        for(char &ch : s) {
-            count[ch-'a']++;
-        }
-        
-        string str;
-        
-        for(int i = 0; i<26; i++) {
-            
-            if(count[i] > 0) {
-                str += string(count[i], i+'a');
-            }
-        }
-        return str;
-    }
+    
     
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-        
         for(string &s : strs) {
-            string s1 = generate(s);
-            
-            mp[s1].push_back(s);
+            string s1 = s;
+            sort(s.begin(),s.end());  
+            mp[s].push_back(s1);
         }
         
         vector<vector<string>> result;

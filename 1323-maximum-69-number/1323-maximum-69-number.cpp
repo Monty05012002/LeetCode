61098752ff596fc79393f99c58591b254ecf447f
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-11-07 14:41:09 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-       string s=to_string(num);
-        for(auto &it:s){
-            if(it=='6'){
-                it='9';
-                break;
-            }
+      int temp=num;
+      int countdigit=-1;
+      int present=-2;
+        while(temp!=0){
+            int rem=temp%10;
+            countdigit++;
+            if(rem==6) present=countdigit;
+            temp=temp/10;
         }
-        return stoi(s);
+        if(present==-2)
+            return num;
+        return num+(3 * pow(10, present));
     }
 };

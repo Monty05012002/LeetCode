d4ca08f0c806e28516e02cbdb5498abe20205e30
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-11-08 21:11:42 +0530

    Time: 4 ms (64.51%), Space: 7.1 MB (8.20%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     string makeGood(string s) {
-        for(int i=1;i<s.size();i++){
-            if(i>=1 && (s[i]-'A'==s[i-1]-'a' || s[i]-'a'==s[i-1]-'A')){
-                s.erase(s.begin()+i-1,s.begin()+i+1);
-                i-=2;
+        stack<char>st;
+        for(char it:s){
+            if(!st.empty()&&(it+32==st.top()||it-32==st.top())){
+                st.pop();
             }
+            else{
+            st.push(it);}
         }
-        return s;
+        string ans;
+        while(!st.empty()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+        return ans;
     }
 };

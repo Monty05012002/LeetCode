825a5a54d3dc547cb590ca0080d1ab442f6b8630
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-12-01 20:40:43 +0530

    Time: 8 ms (40.71%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        int size=s.size()-1;
-        int mid=(0+size)/2;
-        string a=s.substr(0,mid+1);
-        string b=s.substr(mid+1,size+1);
         int count1=0,count2=0;
-        for(int i=0;i<a.size();i++){
-            if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U'){
+        for(int i=0;i<s.size();i++){
+            if(i<s.size()/2){ if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'|s[i]=='O'||s[i]=='U'){
                 count1++;
             }
         }
-          for(int i=0;i<b.size();i++){
-            if(b[i]=='a'||b[i]=='e'||b[i]=='i'||b[i]=='o'||b[i]=='u'||b[i]=='A'||b[i]=='E'||b[i]=='I'||b[i]=='O'||b[i]=='U'){
+    else{        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
                 count2++;
             }
         }
+        }
         return count1==count2;
     }
+               
 };

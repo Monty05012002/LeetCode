2dca284a6ae357e7a31334b7b662c4bf3f6a05c1
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-12-01 22:57:41 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+		int firstOccurence(int nums[],int n,int target){
+        int start=0;
+        int end=n-1;
+        int res=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                res=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else
+                start=mid+1;
+        }
+        return res;
+    }
+     int lastOccurence(int nums[],int n,int target){
+        int start=0;
+        int end=n-1;
+        int res=-1;
+        while(start<=end){
+            int mid=start+((end-start)/2);
+            if(nums[mid]==target){
+                res=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else
+                start=mid+1;
+        }
+        return res;
+    }
+	int count(int nums[], int n, int target) {
+	    // code here
+	    if(n==0)
+	    return 0;
+	    int a=firstOccurence(nums,n,target);
+	    int b=lastOccurence(nums,n,target);
+	    if(a==-1)
+	    return 0;
+	    return b-a+1;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

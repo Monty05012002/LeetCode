23d6455153cc10893f300f71bbb6533665aa0ed6
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2022-12-31 00:34:04 +0530

    Added solution - LeetHub

---
+++ Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to search a given number in row-column sorted matrix.
+    bool search(vector<vector<int> > matrix, int n, int m, int x) 
+    {
+        // code here 
+        int beg=0;
+        int end=m-1;
+        while(beg>=0 && beg<n && end>=0 && end<m){
+            if(matrix[beg][end]==x)
+            return true;
+            if(matrix[beg][end]>x)
+            end--;
+            else
+            beg++;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+    cin>>t;
+    while(t--)
+    {
+
+ 		int n, m;
+    	cin>> n>> m;
+        vector<vector<int> > matrix(n);
+            
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(m, 0);
+            for( int j=0; j<m; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        int x;
+        cin>>x;
+        Solution obj;
+        if (obj.search(matrix,n,m,x)) 
+            cout << "1\n"; 
+        else
+            cout << "0\n"; 
+    }
+	return 0; 
+} 
+
+
+// } Driver Code Ends

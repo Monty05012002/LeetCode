7aa52de674ca04879a76d452cf1ee135783c0785
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-04-13 14:01:06 +0530

    Added solution - LeetHub

--- Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
@@ -10,17 +10,16 @@ class Solution
     int countWays(int n)
     {
         // your code here
-          int a=1,b=1;
-          if(n==1 || n==0)
-          return 1;
-          int c;
-          int mod=1e9+7;
-          for(int i=2;i<=n;i++){
-              c=(a+b)%mod;
-              a=b;
-              b=c;
-          }
-          return c;
+        if(n==1 || n==0 ) return 1;
+        int a=1,b=1,c;
+        int mod=1e9+7;
+        for(int i=2;i<=n;i++){
+            c=(a+b)%mod;
+            a=b;
+            b=c;
+        }
+        return c;
+          
     }
 };
 

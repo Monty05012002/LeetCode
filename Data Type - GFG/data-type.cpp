ed103208c7eb809314e650e7b14f60399903705b
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-04-21 16:07:28 +0530

    Added solution - LeetHub

---
+++ Data Type - GFG/data-type.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// {Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        // your code here
+        if(str=="Character") return 1;
+        else if(str=="Integer" || str=="Float") return 4;
+        else  return 8;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.dataTypeSize(str);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

38bfaca8c3f8ed74e5475ee01192dc56c1507d6b
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-04-22 00:45:37 +0530

    Added solution - LeetHub

--- Count Digits - GFG/count-digits.cpp
+++ Count Digits - GFG/count-digits.cpp
@@ -10,13 +10,8 @@ public:
         int temp=N;
         int count=0;
         while(temp!=0){
-            int r=temp%10;
-            
-            if(r!=0){
-            if(N%r==0){
-                count++;
-            }
-            }
+            int rem=temp%10;
+            if(rem!=0 && N%rem==0) count++;
             temp/=10;
         }
         return count;

4e5cb5c4118cd08316379016ca6ff0ca886c7030
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-05-03 00:27:05 +0530

    Added solution - LeetHub

---
+++ Make Palindrome - GFG/make-palindrome.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    bool makePalindrome(int n,vector<string> &arr){
+        // Code here
+       unordered_map<string,int>mp;
+       for(auto it:arr){
+           mp[it]++;
+           reverse(it.begin(),it.end());
+           mp[it]++;
+       }
+       for(auto it:mp){
+           if(it.second%2==1) return false;
+       }
+       return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        if(ob.makePalindrome(n,arr)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

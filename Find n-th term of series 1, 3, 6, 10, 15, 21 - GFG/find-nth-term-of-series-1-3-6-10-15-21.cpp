a5c4ffa803694cdf733f978a6956494a69dd23c7
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-05-08 20:16:13 +0530

    Added solution - LeetHub

---
+++ Find n-th term of series 1, 3, 6, 10, 15, 21 - GFG/find-nth-term-of-series-1-3-6-10-15-21.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findNthTerm(int N) {
+        // code here
+        if(N==1) return 1;
+        return (N*(N+1))/2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findNthTerm(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

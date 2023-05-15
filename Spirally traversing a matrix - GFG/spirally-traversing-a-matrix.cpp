846bc4a0a2bc9d6b9dd270cbd88f0a02fa46c443
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-05-15 14:28:06 +0530

    Added solution - LeetHub

---
+++ Spirally traversing a matrix - GFG/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        // code here
+        vector<int>ans;
+        int top=0,bottom=r-1,left=0,right=c-1;
+        while(top<=bottom && left<=right){
+            for(int j=left;j<=right;j++){
+                ans.push_back(matrix[top][j]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int j=right;j>=left;j--){
+                    ans.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

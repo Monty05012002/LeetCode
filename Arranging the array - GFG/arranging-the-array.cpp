6149f86050744b0c6b0540faa184da5048993e14
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-06-10 00:47:25 +0530

    Added solution - LeetHub

--- Arranging the array - GFG/arranging-the-array.cpp
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -3,6 +3,32 @@
 using namespace std;
 #define ll long long
 
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            vector<int>pos,neg;
+            for(int i=0;i<n;i++){
+                if(arr[i]<0){
+                    neg.push_back(arr[i]);
+                }else{
+                    pos.push_back(arr[i]);
+                }
+            }
+            int i=0,j=0,k=0;
+            while(j<neg.size()){
+                arr[i++]=neg[j++];
+            }
+            while(k<pos.size()){
+                arr[i++]=pos[k++];
+            }
+        }
+};
+
+//{ Driver Code Starts.
 void Rearrange(int arr[], int n);
 
 int main() 
@@ -17,8 +43,8 @@ int main()
         for(int i=0;i<n;i++)
         cin>>arr[i];
         long long j=0;
-      
-        Rearrange( arr, n);
+        Solution ob;
+        ob.Rearrange(arr, n);
       
         for (int i = 0; i < n; i++) 
             cout << arr[i] << " "; 
@@ -27,28 +53,3 @@ int main()
     return 0; 
 } 
 // } Driver Code Ends
-
-
-
-void Rearrange(int arr[], int n)
-{
-    // Your code goes here
-  
-    vector<int>pos,neg;
-    for(int i=0;i<n;i++){
-        if(arr[i]<0){
-            neg.push_back(arr[i]);
-        }
-        else{
-            pos.push_back(arr[i]);
-        }
-    }
-    for(int i=0;i<pos.size();i++){
-        neg.push_back(pos[i]);
-    }
-    for(int i=0;i<n;i++){
-        arr[i]=neg[i];
-    }
-    
-    
-}

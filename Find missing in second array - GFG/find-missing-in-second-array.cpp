92bca3b366ccac89270328b558749f3f97a9a97f
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-06-12 01:24:45 +0530

    Added solution - LeetHub

---
+++ Find missing in second array - GFG/find-missing-in-second-array.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	vector<long long> findMissing(long long A[], long long B[],  
+                 int N, int M) 
+	{ 
+	    // Your code goes here
+	    unordered_map<long long,long long>mp;
+	    for(int i=0;i<M;i++){
+	        mp[B[i]]++;
+	    }
+	    vector<long long>ans;
+	    for(int i=0;i<N;i++){
+	        if(mp.find(A[i])==mp.end()){
+	            ans.push_back(A[i]);
+	        }
+	    }
+	    return ans;
+	} 
+};
+	
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        long long a[n];
+        long long b[m];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        for(int i=0;i<m;i++)
+        cin >> b[i];
+        vector<long long> ans;
+        
+
+        Solution ob;
+        ans=ob.findMissing(a,b,n,m);
+        for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends

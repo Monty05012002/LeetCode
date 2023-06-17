8617a8c14d91138419185012dc24c71221f1d28b
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-06-17 14:15:08 +0530

    Added solution - LeetHub

--- Queue Operations - GFG/queue-operations.cpp
+++ Queue Operations - GFG/queue-operations.cpp
@@ -10,20 +10,25 @@ using namespace std;
 class Solution{
     public:
     // Function to insert element into the queue
-    unordered_map<int,int>mp;
     void insert(queue<int> &q, int k){
         // Your code here
         q.push(k);
-        mp[k]++;
     }
     
     // Function to find frequency of an element
     // return the frequency of k
     int findFrequency(queue<int> &q, int k){
         // Your code here
-        if(mp.find(k)!=mp.end())
-        return mp[k];
-        return -1;
+        int size=q.size();
+        int count=0;
+        while(size--){
+            int x=q.front();
+            q.pop();
+            if(k==x)
+            count++;
+            q.push(x);
+        }
+        return count==0?-1:count;
     }
     
 };

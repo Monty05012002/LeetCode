2437b208c7f1d0574db8e6e597793075ae53c233
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-08-01 09:38:26 +0530

    Added solution - LeetHub

--- DFS of Graph - GFG/dfs-of-graph.cpp
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -6,21 +6,24 @@ using namespace std;
 class Solution {
   public:
     // Function to return a list containing the DFS traversal of the graph.
-    void dfs(int node,int vis[],vector<int>adj[],vector<int>&ans){
-        vis[node]=1;
-        ans.push_back(node);
-        for(auto it:adj[node]){
+    void solve(int start, vector<int>&vis,vector<int> adj[], vector<int>&ans){
+        vis[start]=1;
+        ans.push_back(start);
+        for(auto it:adj[start]){
             if(!vis[it]){
-                dfs(it,vis,adj,ans);
+                solve(it,vis,adj,ans);
             }
         }
     }
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-        int vis[V]={0};
-        int start=0;
+        vector<int>vis(V,0);
         vector<int>ans;
-        dfs(start,vis,adj,ans);
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                solve(i,vis,adj,ans);
+            }
+        }
         return ans;
     }
 };

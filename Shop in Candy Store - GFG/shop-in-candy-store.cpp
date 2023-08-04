2a8d2ac60c620ea0ca1c17e88c2d612699dc78a5
Monty05012002 <115691866+Monty05012002@users.noreply.github.com>
2023-08-05 01:25:02 +0530

    Added solution - LeetHub

---
+++ Shop in Candy Store - GFG/shop-in-candy-store.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> candyStore(int candies[], int N, int k)
+    {
+        // Write Your Code here
+        sort(candies,candies+N);
+        int mini=0,maxi=0;
+        int c=N;
+        int i=0,j=N-1;
+        while(c>0){
+            mini+=candies[i];
+            maxi+=candies[j];
+            c-=k+1;
+            i++,j--;
+        }
+        return {mini,maxi};
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        int candies[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> candies[i];
+        }
+
+        Solution ob;
+        vector<int> cost = ob.candyStore(candies, N, K);
+        cout << cost[0] << " " << cost[1] << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
